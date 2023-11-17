e57f066cb8ed1f208ea12b22f268355e573661ae
Kelogi <kevin.lopezg@udea.edu.co>
2023-11-16 22:29:44 -0500

    Se crea inicialmemte la clase diamante, para simular una bala que rick dispara

---
+++ diamante.cpp
@@ -0,0 +1,6 @@
+#include "diamante.h"
+
+diamante::diamante()
+{
+
+}
---
+++ diamante.h
@@ -0,0 +1,11 @@
+#ifndef DIAMANTE_H
+#define DIAMANTE_H
+
+
+class diamante
+{
+public:
+    diamante();
+};
+
+#endif // DIAMANTE_H
